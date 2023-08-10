719a73a4e8f2eb66512c42ddc32a3765bb54fce1
sammyrhymes <samuelmwendwa@gmail.com>
2023-08-10 17:03:57 +0300

    Added lower to ensure input is lowercase

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # read input from user
 def readInput():
-    inputLetters = input("Please input the letters:").strip(' ')
+    inputLetters = input("Please input the letters:").strip(' ').lower()
+
     # store in a list
     letters = []
     for letter in inputLetters:
         letters.append(letter)
+
     return letters
-# words
+
+
+# Get words
 def readfromFile():
     fhand = open('words.txt')
     words = []
@@ -14,29 +18,39 @@ def readfromFile():
     for line in fhand:
         word = line.strip('\n')
         words.append(word)
+
     return words
+
+
 # # iterate through it finding only those that are present
 def findWords(words, letters):
     lst = []
+
     for word in words:
-        if len(word) <= len(letters):
+        if len(word) < len(letters)+1:
+
             #  use this as flag
             is_present = True
+
             for letter in letters:
                 if letter not in word:
                     is_present = False
+
             if is_present:
                 lst.append(word)
     return lst
+
+
 # printing the output
 def output(lst):
     count = 0
-    for l in lst:
-        if len(l) < 1:
-           print('no words')
-        else:
-            count += 1
-            print(f"{count} : {l}")
 
-if __name__ == '__main__':
+    if len(lst) < 1:
+        print(f'No words found')
+
+    for l in lst:
+        print(f'{count} : {l}')
+        count += 1
+
+if  __name__ == '__main__':
     output(findWords(readfromFile(), readInput()))
